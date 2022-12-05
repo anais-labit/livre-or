6b13f1fdea63a5b32c76681664ccf429d603d6c0
anais-labit <anais.labit@gmail.com>
2022-12-05 12:45:45 +0100

    ajout du lien de deconnexion

--- includes/footer.php
+++ includes/footer.php
@@ -7,12 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="styles.css" />
-    <title>Footer</title>
 </head>
 
 <body>
 
     <footer>
+        <div class="decon">
+            <a href="./includes/logout.php"> <br> Déconnexion</a>
+        </div>
         <div>
             <p>&copy;<a href="https://anais-labit.students-laplateforme.io/index.html">ANAIS LABIT </a> <span class="year">• 2022</span></p>
         </div>
