f1855bc1645828c8a6089fadd87a7aebddc46e14
anais-labit <anais.labit@gmail.com>
2022-12-05 10:26:03 +0100

    creation du module de connexion à la db

---
+++ includes/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+// connexion à la base de données 
+$conn = new mysqli('localhost', 'root', '', 'livreor');
+// $conn = new mysqli('locahost', 'anais-labit', 'mdpprojets!', 'anais-labit_livreor');
+
+?>
+
