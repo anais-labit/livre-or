d67eb1510ac022e3190dca5cdd15f52d2a2114b8
anais-labit <anais.labit@gmail.com>
2022-12-05 10:40:53 +0100

    cration header et footer

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
+    <title>Header</title>
+</head>
+
+<body>
+
+    <div class="navContainer">
+        <nav>
+            <ul>
+                <li><a href="./includes/index.php">Accueil</a></li>
+                <li><a href="./inscription.php">Inscription</a></li>
+                <li><a href="./connexion.php">Connexion</a></li>
+                <li><a href="./profil.php">Gérer mon profil</a></li>
+                <li><a href="./livre-or.php">Voir les commentaires</a></li>
+                <li><a href="./commentaires.php">Ajouter un commentaire</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</body>
+
+</html>
